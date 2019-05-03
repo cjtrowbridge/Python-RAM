588dbfb69c1b34c859244b55ff7f748cc4ba6c8c
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2019-05-03 14:34:57 -0700

    Update python_ram.py

--- python_ram.py
+++ python_ram.py
@@ -1,7 +1,7 @@
 import random
 import sys
 
-array = []*12000
+ram = []*12000
 bitlist = []
 
 for x in range(12000):
@@ -11,15 +11,39 @@ for x in range(12000):
 		bitlist.append(x)
 		
 	bitstring = ''.join(bitlist)
-	array.append(bitstring)
+	ram.append(bitstring)
 	
 #read and display the hundredth record
-_100th = array[99]
+_100th = ram[99]
 print('hundredth element: ' + _100th)
 
 #read and display the thousandth record
-_1000th = array[999]
+_1000th = ram[999]
 print('thousandth element: ' + _1000th)
+
+#overwrite the 99th and 999th bytes with zeroes
+blankByte = [0,0,0,0,0,0,0,0]
+ram = replace(ram,blankByte,99)
+ram = replace(ram,blankByte,999)
+
+#read and display the hundredth record
+_100th = ram[99]
+print('overwritten hundredth element: ' + _100th)
+
+#read and display the thousandth record
+_1000th = ram[999]
+print('overwritten thousandth element: ' + _1000th)
 
 print("Your array can hold a max record of: " , sys.maxsize)
 print('\n')
+
+#print alternate elements
+i = 0
+while(i < len(ram)):
+	print(ram[i], end = " ")
+	i = i + 2
+	
+	
+		
+	
+	
