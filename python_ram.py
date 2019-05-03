361693c699c4055b76eba3e5acbc9a9e152ad759
EC2 Default User <ec2-user@ip-172-31-45-28.ec2.internal>
2019-05-03 23:37:04 +0000

    Implemented functions.

--- python_ram.py
+++ python_ram.py
@@ -1,49 +1,38 @@
 import random
 import sys
+import string
 
 ram = []*12000
 bitlist = []
+
+def displayRecords(prepend =''):
+	hundredth  = ''.join(str(v) for v in ram[99])
+	thousandth = ''.join(str(v) for v in ram[999])
+	print(prepend + 'hundredth element: '  + hundredth)
+	print(prepend + 'thousandth element: ' + thousandth)
+
+def blankOut(byteNumber):
+	ram[byteNumber]='00000000'
 
 for x in range(12000):
 	bitlist = []
 	for i in range(0,8):
 		x = str(random.randint(0,1))
 		bitlist.append(x)
-		
 	bitstring = ''.join(bitlist)
 	ram.append(bitstring)
 	
-#read and display the hundredth record
-_100th = ram[99]
-print('hundredth element: ' + _100th)
-
-#read and display the thousandth record
-_1000th = ram[999]
-print('thousandth element: ' + _1000th)
+displayRecords()
 
 #overwrite the 99th and 999th bytes with zeroes
-blankByte = [0,0,0,0,0,0,0,0]
-ram = replace(ram,blankByte,99)
-ram = replace(ram,blankByte,999)
+blankOut(99)
+blankOut(999)
 
-#read and display the hundredth record
-_100th = ram[99]
-print('overwritten hundredth element: ' + _100th)
-
-#read and display the thousandth record
-_1000th = ram[999]
-print('overwritten thousandth element: ' + _1000th)
-
-print("Your array can hold a max record of: " , sys.maxsize)
-print('\n')
+displayRecords('overwritten ')
+print('The array can hold a max of 1500 8-byte words for a total size of 12,000 bits.\n')
 
 #print alternate elements
-i = 0
+i = 1
 while(i < len(ram)):
-	print(ram[i], end = " ")
+	print(str(i)+":"+str(ram[i]) + " "),
 	i = i + 2
-	
-	
-		
-	
-	
